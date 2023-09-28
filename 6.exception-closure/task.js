152d7d4aad25f5dbf19aa51daed1d4d5dda36cf1
Alexey.K <lexxtlt@gmail.com>
2023-09-28 19:43:23 +0400

    Задания выполнены

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,65 @@
-﻿
+﻿// задача 1
+
+function parseCount(numberToParse) {
+  if (isNaN(numberToParse)) {
+    throw new Error("Невалидное значение")
+  }
+    return Number.parseFloat(numberToParse);
+  }
+
+
+function validateCount(numberToParse) {
+    try {
+        return parseCount(numberToParse);
+    }
+    catch (error) {
+        return error;
+    }    
+}
+
+
+// Задача 2
+
+class Triangle{
+    constructor(a, b, c) {
+        if ((a + b) < c || (a +c) < b || (b + c) < a) {
+            throw new Error('Треугольник с такими сторонами не существует')
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const halfPerimeter = this.perimeter / 2
+        return Number(Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3)) ;
+    }
+}
+
+
+
+function getTriangle (a, b, c) {
+    try {
+        let trg = new Triangle(a, b, c);
+        return trg;
+    }
+    catch (error) {
+        let err = {
+            get perimeter() {
+               return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+               return "Ошибка! Треугольник не существует";
+            }
+        }
+        return err;
+    }
+    finally {
+
+    }
+}
